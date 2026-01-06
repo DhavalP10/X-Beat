6b22d314a17002b40e2e0b6517881629ec63a64a
Panchal Dhaval <dhavalxd10@gmail.com>
2026-01-06 16:20:29 +0530

    api called

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -17,10 +17,20 @@ const [activeTab, setActiveTab] = useState("specifications");
 const [mainImage, setMainImage] = useState("");
 const navigate = useNavigate();
 const { products, loading } = useContext(ProductContext);
-if (!id) {
+if (!id) return null;
+
+if (loading) {
   return (
     <div className="min-h-screen bg-[#121212] text-gray-400 flex items-center justify-center">
-      Invalid product
+      Loading product details...
+    </div>
+  );
+}
+
+if (!product) {
+  return (
+    <div className="min-h-screen bg-[#121212] text-gray-400 flex items-center justify-center">
+      Product not found
     </div>
   );
 }
@@ -48,13 +58,7 @@ useEffect(() => {
     setRelatedProducts(related);
   }, [product, products]);
 
-  if (loading || !product) {
-    return (
-      <div className="min-h-screen bg-[#121212] text-gray-400 flex items-center justify-center">
-        Loading product details...
-      </div>
-    );
-  }
+  
 
 
 
